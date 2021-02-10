605fb2e6e8427a852b965d956abfcf633798f839
s55ye <sophia.ye@uwaterloo.ca>
2021-02-09 20:14:41 -0500

    Updated TCB with *u_stack_hi field, removed previously added depricated TCB fields

--- manual_code/lab2/TM/src/kernel/k_inc.h
+++ manual_code/lab2/TM/src/kernel/k_inc.h
@@ -69,8 +69,7 @@ typedef struct tcb {
     U8          prio;   /**> Execution priority                         */
     U8          state;  /**> task state                                 */
     U8          priv;   /**> = 0 unprivileged, =1 privileged            */
-    U32         k_sp;           // kernel-space stack pointer
-    U32         u_sp;           // user-space stack pointer
+    U32        *u_stack_hi;     // user-space stack start
     U16         u_stack_size;   // user-space stack size
     void        (*ptask)();     // task entry address
 } TCB;
